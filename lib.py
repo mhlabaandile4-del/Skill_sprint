b761b950bf83fdb36ba22d03a808575d67f0e845
mhlabaandile4-del <mhlabaandile4@gmail.com>
2026-09-10 17:41:14 -0700

    Add files via upload
    Its 3am, I'm tired

--- lib.py
+++ lib.py
@@ -1,23 +1,39 @@
+import pyrebase
+firebaseConfig = {'apiKey': "AIzaSyD1tM5MWpg09sYYYAX0IO8ifdp7QG-SYUA",
+  'authDomain': "skillsprint-cf16f.firebaseapp.com",
+  'databaseURL': "https://console.firebase.google.com/project/skillsprint-cf16f/database/skillsprint-cf16f-default-rtdb/data/~2F",
+  'projectId': "skillsprint-cf16f",
+  'storageBucket': "skillsprint-cf16f.firebasestorage.app",
+  'messagingSenderId': "137896376970",
+  'appId': "1:137896376970:web:6cf8b0d255e2981c100b73",
+  'measurementId': "G-92Y6Y9J966"}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+auth = firebase.auth()
+
+
 #STUDENT CLASS
 class Student:
-    def __init__(self, full_name, campus, email, password, short_bio, repo_link, rank):
-        self.full_name = full_name
+    def __init__(self, user_name, campus, email, password, short_bio, repo_link, rank):
+        self.user_name = user_name
         self.campus    = campus
         self.email     = email
         self.password  = password
         self.short_bio = short_bio
         self.repo_link = repo_link
         self.rank      = rank
-
+    
     
 #STUDENT CLASS
 #LECTURER CLASS   
 class Lecturer:
-    def __init__(self, full_name, campus, email, password, short_bio):
-        self.full_name = full_name
+    def __init__(self, user_name, campus, email, password, short_bio):
+        self.user_name = user_name
         self.campus    = campus
         self.email     = email
         self.password  = password
         self.short_bio = short_bio
+
+
 #LECTURER CLASS
-        
+#skillsprint-cf16f
--- main.py
+++ main.py
@@ -1,8 +1,94 @@
-import lib
+import lib as lib
 import streamlit as st
 
 #I'm planning to create a simple modal that asks for user information and stores it in firebase, add a simple profile view later
-st.title("Welcome to the Streamlit App")
 #JUANDRE will deal with Ai api intergration, just make relaible and fast, add simple a one week timer variable and prompt the API to return a simple "task for the week" and store it in TASK variable 
 #SASHA and MAXWELL will deal with the simple leader board, add a simple leaderboard that shows the top 10 students based on their rank, and make sure to update it in real-time as students complete tasks.
 #This a collaboration project, so anyone is welcome to ask for help.
+
+successful_log_sign = False #If true, the main page will be displayed with some user information
+
+#SIGN_UP/LOGIN MODAL
+
+def valide():
+    pass
+
+def sign_up_login_modal():
+    st.title("SkillSprint")
+
+    lgnorsgn  = st.radio("New here ?", options = ("Yes", "No")) 
+    if lgnorsgn == "Yes":
+        #SIGNUP MODAL
+        st.subheader("Sign up")
+        sgn_form   = st.form("sign up")
+        username  = sgn_form.text_input("Username :")
+        email     = sgn_form.text_input("Email :", type="email")
+        password  = sgn_form.text_input("Password :", type="password")
+        confirm_password  = sgn_form.text_input("Confirm password :", type="password")
+        campus    = sgn_form.radio("What campus are you from : ", options= ("Newtown","Brynston","Umhlanga"))
+        short_bio = sgn_form.text_area("Tell us about yourself", max_chars= 100)
+        student_repo_link = sgn_form.text_input("Link to your student repository :")
+        student_or_lecturer = sgn_form.radio("Are you a student or a lecturer ?", options= ("Student","Lecturer"))
+        #Simple form validation
+        form_valid = False
+        if sgn_form.form_submit_button("Submit"):
+            if username == "":
+                  st.warning("Please enter name")
+                  form_valid = False
+            elif any(cha.isdigit() for cha in username):
+                    st.warning("Username cannot contain numbers")
+                    form_valid = False
+            elif len(username) < 5:
+                    st.warning("Username must be at least 5 characters long")
+                    form_valid = False
+            elif email == "":
+                    st.warning("Please enter email")
+                    form_valid = False
+            elif password == "":
+                    st.warning("Please enter password")
+                    form_valid = False
+            elif len(password) < 8:
+                    st.warning("Password must be at least 8 characters long")
+                    form_valid = False
+            elif password != confirm_password:
+                    st.warning("Passwords do not match")
+                    form_valid = False
+            if short_bio == "":
+                    st.warning("Please enter a short bio")
+                    form_valid = False
+            if student_repo_link == "":
+                    st.warning("Please enter a link to your student repository")
+                    form_valid = False
+            else:
+                form_valid = True
+                st.success("Form submitted successfully")      
+                st.success("User created successfully")
+                st.info("Please login to continue")
+                st.session_state['successful_log_sign'] = True
+            #Simple form validation
+            #Adds account to firebase, creates user object, and store user information in firebase database and loads main page   
+            if form_valid:
+                  New_user = lib.Student(username, campus, email, password, short_bio, student_repo_link, 0)
+                  lib.auth.create_user_with_email_and_password(email, password)
+                  st.session_state['user'] = New_user
+                   
+    else:
+        st.subheader("Login")
+        lg_form   = st.form("login")
+        username  = lg_form.text_input("Username :")
+        password  = lg_form.text_input("Password :", type="password")
+        #Check if user exists in firebase database and validate password, if valid load main page, else show error message
+        if lg_form.form_submit_button("Submit"):
+            try:
+                user = lib.auth.sign_in_with_email_and_password(username, password)
+                st.session_state['successful_log_sign'] = True
+                st.success("Login successful")
+                st.session_state['user'] = user
+            except:
+                st.error("Invalid username or password")
+
+sign_up_login_modal()
+
+#SIGN_UP/LOGIN MODAL
+#MAINPAGE
+#MAINPAGE
